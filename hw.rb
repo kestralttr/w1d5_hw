b91a41edcccaafac530f2fa9323eaef0d828663f
Alex Bennett <kestralttr@Alex-Bennetts-MacBook-Pro.local>
2016-09-15 22:05:30 -0700

    adds hw answers

--- hw.rb
+++ hw.rb
@@ -1,1 +1,65 @@
 #first pushed hw
+
+class Stack
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    @stack << el
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    @stack
+  end
+end
+
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.unshift(el)
+  end
+
+  def dequeue
+    @queue.pop
+  end
+
+  def show
+    @queue
+  end
+
+end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(k,v)
+    @map << [k,v]
+  end
+
+  def lookup(k)
+    @map.each do |key|
+      return key[1] if key[0] == k
+    end
+  end
+
+  def remove(k)
+    @map.each_with_index do |el, idx|
+      @map.delete_at(idx) if el[0] == k
+    end
+  end
+
+  def show
+    @map
+  end
+
+end
